6c13f4f604f474e096bc70cc0defee1caba4250d
Strider <fernando.herrera85@gmail.com>
2017-07-20 12:17:00 -0600

    Inicio del commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import {AjustesPage,Ajustes2Page,ModalPage,
+        TabsPage,Pagina2Page,Pagina3Page,PrincipalPage} from "../pages/index.paginas";
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    TabsPage,
+    Pagina2Page,
+    Pagina3Page,
+    PrincipalPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +28,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    TabsPage,
+    Pagina2Page,
+    Pagina3Page,
+    PrincipalPage
   ],
   providers: [
     StatusBar,
